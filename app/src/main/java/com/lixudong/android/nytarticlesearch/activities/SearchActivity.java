16305c05bb62ff51e1bbc93108638b12a3a0c1a2
XudongLi <xudongli@uw.edu>
2016-10-22 22:47:34 -0700

    The most basic workable version of this app.

---
+++ app/src/main/java/com/lixudong/android/nytarticlesearch/activities/SearchActivity.java
@@ -0,0 +1,89 @@
+package com.lixudong.android.nytarticlesearch.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import com.lixudong.android.nytarticlesearch.Article;
+import com.lixudong.android.nytarticlesearch.ArticleArrayAdapter;
+import com.lixudong.android.nytarticlesearch.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class SearchActivity extends AppCompatActivity {
+
+    EditText etQuery;
+    GridView gvResults;
+    Button btnSearch;
+
+    ArrayList<Article> articles;
+    ArticleArrayAdapter articleArrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        setupViews();
+    }
+
+    public void setupViews() {
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        gvResults = (GridView) findViewById(R.id.gvResult);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        articles = new ArrayList<>();
+        articleArrayAdapter = new ArticleArrayAdapter(this, articles);
+        gvResults.setAdapter(articleArrayAdapter);
+
+        gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(), ArticleActivity.class);
+                Article article = articles.get(position);
+                i.putExtra("article", article);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void onArticleSearch(View view) {
+        String query = etQuery.getText().toString();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        RequestParams requestParams = new RequestParams();
+        requestParams.put("api-key", "7494f6c4d819449789f4d52bc20cada9");
+        requestParams.put("page", 0);
+        requestParams.put("q", query);
+        Log.d("DEBUG", requestParams.toString());
+
+        client.get(url, requestParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray articleJsonResults = null;
+
+                try {
+                    articleJsonResults = response.getJSONObject("response").getJSONArray("docs");
+                    articleArrayAdapter.addAll(Article.fromJSONArray(articleJsonResults));
+                    Log.d("DEBUG", articleJsonResults.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
